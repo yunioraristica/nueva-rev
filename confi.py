4ff605bab2f6a8df6abaf385591159d1f5f9f367
yunioraristica <102574994+yunioraristica@users.noreply.github.com>
2025-12-19 09:17:04 -0500

    Update confi.py

--- confi.py
+++ confi.py
@@ -1,3 +1,4 @@
-Bot_token = "5935019057:AAGC8UersIuBWo2cNvYMgXHnULdO1vT-rt8"
+Bot_token = "8546436958:AAGfWjBOPBHuZiPRnxF6i0CJar61ftpnaZ0"
 chanel_id =  -1001910694479
+
 db_access = 2 #value int de la db, desmarcar line 1531 bot.py
